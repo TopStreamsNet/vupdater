9d658de7ea550537fbc2406105f88c45d5c9d6ae
Boshaw <boshaw@protonmail.com>
2016-10-14 17:33:55 -0400

    updater for ape

---
+++ src/ape/FileIO.java
@@ -0,0 +1,16 @@
+package ape;
+
+import java.util.*;
+import java.io.*;
+
+public class FileIO {
+    public static String[] loadIniFile(String f) throws Exception {
+	ArrayList<String> str = new ArrayList<String>();
+	String ln;
+	BufferedReader br = new BufferedReader(new FileReader(f));
+	while((ln = br.readLine()) != null)
+	    str.add(ln);
+	return str.toArray(new String[0]);
+    }
+}
+
